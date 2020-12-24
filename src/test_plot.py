70101918edfd241a014f58a0338361b7cdb218a6
lkmatsuda12 <70863630+lkmatsuda12@users.noreply.github.com>
2020-12-24 00:55:27 -0300

    Add files via upload
    files with the plot functions and an initial idea to implement them in the main code

---
+++ src/test_plot.py
@@ -0,0 +1,32 @@
+# obtain all the functions from the file functions.py
+from functions import*
+
+
+# dictionary that will provide data to portfolio's performance graphic 
+historic = {'Period':[],
+             'Return':[0.02, 0.08, -0.01, -0.02]
+    }
+
+
+# dictionary that will provide data to portfolio's composition plot 
+portfolio = {'Stock':['ITUB4','PSSA3','EGIE3'],
+             'Amount':[140,60,67],
+             'Total Value':[3410.53, 2999.8, 2688.73],
+             'Average Value':[]
+}
+                
+
+# variable to be used in the telegram's function. plot and save the portfolio's composition 
+#securities = img_portfolio(portfolio['Total Value'], portfolio['Stock'])
+
+# variable to be used in the telegram's function. plot and save the portfolio's performance
+#performance = pic_portfolio_performance(historic["Return"])
+
+
+
+                     
+                     
+
+
+#send_image("1496681905:AAGNUmXBHLpXRN4VkcFao1emrBUKW1bRMEs", " composicao ou performance","-356143379" )
+
