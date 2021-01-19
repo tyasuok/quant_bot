9a0339291e3248c1e0b9046bf51d4791e13cc71c
Thomas Yasuoka <thomas.yasuoka@outlook.com>
2021-01-19 19:14:08 -0300

    added new passwords_blank.py, without the telegram bot info

---
+++ src/passwords_blank.py
@@ -0,0 +1,20 @@
+test = {
+    "login": "LOGIN",
+    "passw": "SENHA",
+    "server": "Rico-PRD"
+}
+
+rico = {
+    "login": "LOGIN",
+    "passw": "SENHA"
+    "server": "Rico-PRD"
+}
+
+rico_demo = {
+    "login": "LOGIN",
+    "passw": "SENHA",
+    "server": "Rico-Demo"
+}
+
+TELE_TOKEN = "TOKEN" # o token do bot
+CHAT_ID = "ID DO GRUPO" # id do grupo q eu fiz pra testar
